9c2a04bd8cad64768b928bfdafd7085d18f95c62
Aiden Storey <git@storey.dev>
2021-11-13 14:53:19 -0500

    Added player movement

--- source/main.gd
+++ source/main.gd
@@ -29,35 +29,45 @@ func _ready() -> void:
 
 			self.__squares.append(instance)
 
-
 	var player_reference: Resource = preload("res://source/player.tscn")
 
 	var instance: Player = player_reference.instance()
 	instance.position = self.__initial_position + Globals.PLAYER_OFFSET
+	instance.set_direction(Vector2.DOWN)
+	instance.set_can_move_callback(funcref(self, "__can_move"))
 
 	self.call_deferred("add_child", instance)
 
 	Event.connect("player_landed", self, "__player_landed")
 
 
-#var test_time_elapsed: float = 0.5
-#
-#func _process(delta: float) -> void:
-#	test_time_elapsed = max(0.0, test_time_elapsed - delta)
-#
-#	if test_time_elapsed == 0.0:
-#		test_time_elapsed = 0.5
-#
-#		var index: int = randi() % 100
-#
-#		self.__squares[index].land()
-
-
 # Private methods
 
-func __player_landed(player: Player) -> void:
-	var relative_position: Vector2 = player.position - self.__initial_position - Globals.PLAYER_OFFSET
-	var indexed_position: Vector2 = relative_position / Globals.SQUARE_SIZE
-	var index: int = indexed_position.y * self.size + indexed_position.x
+func __can_move(origin, destination) -> bool:
+	var index_origin: Vector2 = self.__position_to_index_position(origin)
+	var index_destination: Vector2 = self.__position_to_index_position(destination)
 
+	if index_destination.x < 0.0 || index_destination.x >= size || \
+		index_destination.y < 0.0 || index_destination.y >= size:
+			return false
+
+	var delta = (index_destination - index_origin).snapped(Vector2(0.1, 0.1))
+	if ![Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN].find(delta):
+		return false
+
+	return true
+
+
+func __player_landed(player: Player) -> void:
+	var index: int = self.__position_to_index(player.position)
 	self.__squares[index].land(player)
+
+
+func __position_to_index_position(incoming: Vector2) -> Vector2:
+	var relative_position: Vector2 = incoming - self.__initial_position - Globals.PLAYER_OFFSET
+	return relative_position / Globals.SQUARE_SIZE
+
+
+func __position_to_index(incoming: Vector2) -> int:
+	var indexed_position: Vector2 = self.__position_to_index_position(incoming)
+	return int(indexed_position.y * self.size + indexed_position.x)
--- source/player.gd
+++ source/player.gd
@@ -1,11 +1,21 @@
 class_name Player extends Node2D
 
 
+# Public variables
+
+export(float) var move_delta: float = 0.0 setget __set_move_delta, __get_move_delta
+
+
 # Private variables
 
 onready var __animation: AnimationPlayer = $animation
 
+var __destination: Vector2 = Vector2.ZERO
+var __origin: Vector2 = Vector2.ZERO
+var __should_move: bool = false
+
 var __direction: Vector2 = Vector2.ZERO
+var __can_move_callback: FuncRef = null
 
 
 # Lifecylce method
@@ -25,4 +35,41 @@ func land() -> void:
 
 
 func move() -> void:
-	pass
+	self.__origin = self.position
+
+	if self.__can_move_callback == null:
+		self.__destination = self.__origin
+		return
+
+	self.__destination = self.global_position + self.__direction * Globals.SQUARE_SIZE
+
+	if !self.__can_move_callback.call_func(self.__origin, self.__destination):
+		self.__destination = self.__origin
+
+	self.__should_move = true
+
+
+func set_can_move_callback(incoming: FuncRef) -> void:
+	self.__can_move_callback = incoming
+
+
+func set_direction(incoming: Vector2) -> void:
+	self.__direction = incoming
+
+
+# Private methods
+
+func __get_move_delta() -> float:
+	return move_delta
+
+
+func __set_move_delta(incoming: float) -> void:
+	move_delta = incoming
+
+	if !self.__should_move:
+		return
+
+	self.position = lerp(self.__origin, self.__destination, move_delta)
+
+	self.__should_move = self.position != self.__destination
+
