b847c8f0e2f71e56df7a87bc832395fd8f54d5fd
Aiden Storey <git@storey.dev>
2021-11-19 12:21:12 -0500

    Add buttons to menu

--- source/menus/menu_start.gd
+++ source/menus/menu_start.gd
@@ -4,6 +4,11 @@ extends Control
 # Private methods
 
 onready var __animation: AnimationPlayer = $animation
+onready var __button_play: Button = $play
+onready var __button_tutorial: Button = $tutorial
+onready var __button_settings: Button = $settings
+onready var __button_exit: Button = $exit
+
 
 
 # Lifecylce methods
@@ -12,4 +17,5 @@ func _ready() -> void:
 	self.__animation.play("load")
 	yield(self.__animation, "animation_finished")
 
+	self.__button_play.grab_focus()
 	self.__animation.play("change")
